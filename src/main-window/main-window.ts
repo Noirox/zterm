3eaaf3157a78b0017be2a24c3401f5ee0f8fa275
Norox <err666@gmail.com>
2018-02-22 22:47:58 +0100

    few adjustments

--- src/main-window/main-window.ts
+++ src/main-window/main-window.ts
@@ -1,9 +1,9 @@
-const { app, BrowserWindow } = require("electron")
-const WindowStateManager = require("electron-window-state-manager")
-import { loadURL } from "./load-url"
+const {app, BrowserWindow} = require("electron");
+const WindowStateManager = require("electron-window-state-manager");
+import {loadURL} from "./load-url";
 
 // default dimensions
-export const DIMENSIONS = { width: 600, height: 500, minWidth: 450, minHeight: 450 }
+export const DIMENSIONS = {width: 1000, height: 800, minWidth: 900, minHeight: 800};
 
 /**
  * Creates the main window.
@@ -17,7 +17,7 @@ export function createMainWindow(appPath: string, showDelay: number = 100) {
   const windowState = new WindowStateManager("main", {
     defaultWidth: DIMENSIONS.width,
     defaultHeight: DIMENSIONS.height,
-  })
+  });
 
   // create our main window
   const window = new BrowserWindow({
@@ -29,38 +29,39 @@ export function createMainWindow(appPath: string, showDelay: number = 100) {
     y: windowState.y,
     show: false,
     useContentSize: true,
-    titleBarStyle: "hidden-inset",
+    //titleBarStyle: "hidden-inset",
     autoHideMenuBar: true,
     // backgroundColor: '#fff',
     vibrancy: "light",
-    transparent: true,
+    transparent: false,
     title: app.getName(),
+    frame: true,
     webPreferences: {
       backgroundThrottling: false,
       textAreasAreResizable: false,
     },
-  })
+  });
 
   // maximize if we did before
   if (windowState.maximized) {
-    window.maximize()
+    window.maximize();
   }
 
   // trap movement events
-  window.on("close", () => windowState.saveState(window))
-  window.on("move", () => windowState.saveState(window))
-  window.on("resize", () => windowState.saveState(window))
+  window.on("close", () => windowState.saveState(window));
+  window.on("move", () => windowState.saveState(window));
+  window.on("resize", () => windowState.saveState(window));
 
   // load entry html page in the renderer.
-  loadURL(window, appPath)
+  loadURL(window, appPath);
 
   // only appear once we've loaded
   window.webContents.on("did-finish-load", () => {
     setTimeout(() => {
-      window.show()
-      window.focus()
-    }, showDelay)
-  })
+      window.show();
+      window.focus();
+    }, showDelay);
+  });
 
-  return window
+  return window;
 }
