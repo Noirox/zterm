e8e4089e48a00d778a01ed2ee5b32cd094994b0d
Noirox <err666@gmail.com>
2018-02-22 16:48:38 +0100

    add react mosaic nad blueprintjs styles

--- fuse.ts
+++ fuse.ts
@@ -1,30 +1,30 @@
-import { FuseBox, CSSPlugin, Sparky, CopyPlugin, ReplacePlugin } from "fuse-box"
-import { spawn } from "child_process"
-import * as pjson from "./package.json"
+import { FuseBox, CSSPlugin, Sparky, CopyPlugin, ReplacePlugin } from 'fuse-box'
+import { spawn } from 'child_process'
+import * as pjson from './package.json'
 
 const DEV_PORT = 4445
-const OUTPUT_DIR = "out"
-const ASSETS = ["*.jpg", "*.png", "*.jpeg", "*.gif", "*.svg"]
+const OUTPUT_DIR = 'out'
+const ASSETS = ['*.jpg', '*.png', '*.jpeg', '*.gif', '*.svg', '*.css']
 
 // are we running in production mode?
-const isProduction = process.env.NODE_ENV === "production"
+const isProduction = process.env.NODE_ENV === 'production'
 
 // copy the renderer's html file into the right place
-Sparky.task("copy-html", () => {
-  return Sparky.src("src/app/index.html").dest(`${OUTPUT_DIR}/$name`)
+Sparky.task('copy-html', () => {
+  return Sparky.src('src/app/index.html').dest(`${OUTPUT_DIR}/$name`)
 })
 
 // the default task
-Sparky.task("default", ["copy-html"], () => {
+Sparky.task('default', ['copy-html'], () => {
   // setup the producer with common settings
   const fuse = FuseBox.init({
-    homeDir: "src",
+    homeDir: 'src',
     output: `${OUTPUT_DIR}/$name.js`,
-    target: "electron",
+    target: 'electron',
     log: isProduction,
     cache: !isProduction,
     sourceMaps: true,
-    tsConfig: "tsconfig.json",
+    tsConfig: 'tsconfig.json',
   })
 
   // start the hot reload server
@@ -34,13 +34,13 @@ Sparky.task("default", ["copy-html"], () => {
 
   // bundle the electron main code
   const mainBundle = fuse
-    .bundle("main")
-    .target("server")
-    .instructions("> [app/main.ts]")
+    .bundle('main')
+    .target('server')
+    .instructions('> [app/main.ts]')
     // inject in some configuration
     .plugin(
       ReplacePlugin({
-        "process.env.HOMEPAGE": pjson.homepage ? `"${pjson.homepage}"` : "null",
+        'process.env.HOMEPAGE': pjson.homepage ? `"${pjson.homepage}"` : 'null',
       }),
     )
 
@@ -51,10 +51,10 @@ Sparky.task("default", ["copy-html"], () => {
 
   // bundle the electron renderer code
   const rendererBundle = fuse
-    .bundle("renderer")
-    .instructions("> [app/index.tsx] +fuse-box-css")
+    .bundle('renderer')
+    .instructions('> [app/index.tsx] +fuse-box-css')
     .plugin(CSSPlugin())
-    .plugin(CopyPlugin({ useDefault: false, files: ASSETS, dest: "assets", resolve: "assets/" }))
+    .plugin(CopyPlugin({ useDefault: false, files: ASSETS, dest: 'assets', resolve: 'assets/' }))
 
   // and watch & hot reload unless we're bundling for production
   if (!isProduction) {
@@ -66,9 +66,9 @@ Sparky.task("default", ["copy-html"], () => {
   return fuse.run().then(() => {
     if (!isProduction) {
       // startup electron
-      spawn("node", [`${__dirname}/node_modules/electron/cli.js`, __dirname], {
-        stdio: "inherit",
-      }).on("exit", code => {
+      spawn('node', [`${__dirname}/node_modules/electron/cli.js`, __dirname], {
+        stdio: 'inherit',
+      }).on('exit', code => {
         console.log(`electron process exited with code ${code}`)
         process.exit(code)
       })
--- src/app/root-component.tsx
+++ src/app/root-component.tsx
@@ -1,22 +1,51 @@
 // This is the top-most component in the app.
-import * as React from "react"
-import { compose } from "glamor"
-import { styles, colors } from "../views/theme"
-import { WelcomeScreen } from "../views/example/welcome-screen"
+import * as React from 'react'
+import { compose } from 'glamor'
+import { colors, styles } from '../views/theme'
+import { Mosaic, MosaicWindow } from 'react-mosaic-component'
+
+import './react-mosaic-component.css'
 
 const ROOT = compose(styles.fullScreen, {
   background: colors.window.background,
-  "& ::-webkit-scrollbar": { backgroundColor: colors.scrollbar.base, width: 12, height: 12 },
-  "& ::-webkit-scrollbar-track": { backgroundColor: colors.scrollbar.track },
-  "& ::-webkit-scrollbar-thumb": { backgroundColor: colors.scrollbar.thumb, borderRadius: 4 },
+  '& ::-webkit-scrollbar': { backgroundColor: colors.scrollbar.base, width: 12, height: 12 },
+  '& ::-webkit-scrollbar-track': { backgroundColor: colors.scrollbar.track },
+  '& ::-webkit-scrollbar-thumb': { backgroundColor: colors.scrollbar.thumb, borderRadius: 4 },
 })
+
+export type ViewId = 'a' | 'b' | 'c' | 'new'
+
+// Make a simple extension class to preserve generic type checking in TSX
+class ViewIdMosaic extends Mosaic<ViewId> {}
+
+class ViewIdMosaicWindow extends MosaicWindow<ViewId> {}
+
+const TITLE_MAP: Record<ViewId, string> = {
+  a: 'Left Window',
+  b: 'Top Right Window',
+  c: 'Bottom Right Window',
+  new: 'New Window',
+}
 
 export class RootComponent extends React.Component<{}, {}> {
   render() {
     return (
-      <div {...ROOT}>
-        <WelcomeScreen />
-      </div>
+      <ViewIdMosaic
+        renderTile={(id, path) => (
+          <ViewIdMosaicWindow path={path} createNode={() => 'new'} title={TITLE_MAP[id]}>
+            <h1>{TITLE_MAP[id]}</h1>
+          </ViewIdMosaicWindow>
+        )}
+        initialValue={{
+          direction: 'row',
+          first: 'a',
+          second: {
+            direction: 'column',
+            first: 'b',
+            second: 'c',
+          },
+        }}
+      />
     )
   }
 }
