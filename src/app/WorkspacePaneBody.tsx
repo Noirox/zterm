c57263d4639b2e8eb9762c41847383a0591b43ab
Norox <err666@gmail.com>
2018-02-24 16:59:50 +0100

    design refactor

--- src/app/WorkspacePaneBody.tsx
+++ src/app/WorkspacePaneBody.tsx
@@ -1,13 +1,14 @@
 import {css} from 'glamor';
 import * as React from 'react';
+import {theme} from './theme';
 
 const workspacePaneBodyStyles = css({
   height: '100vh',
-  width: '100vw',
+  width: `calc(100vw - ${theme.sideNavbar.width})`,
   position: 'absolute',
   left: 0,
   top: 0,
-  margin: '50px 0 0 50px',
+  margin: `${theme.workspace.header.height} 0 0 ${theme.sideNavbar.width}`,
   backgroundColor: '#2B303B',
   padding: '5px',
   //borderTop: '1px solid black'
