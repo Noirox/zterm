7ede7f8acfb167a0c632fd55279b12a31d9373b7
Norox <err666@gmail.com>
2018-02-25 11:05:26 +0100

    calc proper height ¯\_(ツ)_/¯

--- src/app/Settings.Panel.tsx
+++ src/app/Settings.Panel.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 export const SettingsPanel = (props: ISettingsPanelProps) => {
   const {title, icon} = props;
   return (
-    <div style={{width: '100%', padding: '25px 0 0 0'}}>
+    <div style={{width: '100%', height: 'calc(100vh - 74px)', overflowY: 'auto', padding: '25px 0 0 0'}}>
       <h3>
         <SettingsPanelTitle title={title} icon={icon} />
       </h3>
