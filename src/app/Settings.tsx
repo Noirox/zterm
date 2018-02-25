e29e54d22434df5462e351cda06588a8c7c74c8c
Norox <err666@gmail.com>
2018-02-25 02:01:09 +0100

    Settings tab

--- src/app/Settings.tsx
+++ src/app/Settings.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 import {TabModel} from './TabModel';
+import {Switch} from '@blueprintjs/core';
 
 export class Settings extends React.Component {
+  state = {
+    showWelcome: true,
+  };
+
+  handlePublicChange = () => this.setState({showWelcome: !this.state.showWelcome});
+
+  
   render() {
-    return (<div>inside settings component</div>);
+    const {showWelcome} = this.state;
+    return (
+      <div>
+        inside settings component
+        <Switch
+          checked={showWelcome}
+          label='Show welcome page when opening YATE-3270-'
+          onChange={this.handlePublicChange}
+        />
+      </div>);
   }
 }
 
